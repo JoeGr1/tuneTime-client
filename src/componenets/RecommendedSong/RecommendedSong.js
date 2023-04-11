e96d534027c2a4637b3ef2840b823faac5b9b9e5
Joe Gregson <joegregson@icloud.com>
2023-04-11 13:25:02 +0100

    style recomm song card

--- src/componenets/RecommendedSong/RecommendedSong.js
+++ src/componenets/RecommendedSong/RecommendedSong.js
@@ -1,12 +1,29 @@
 import React from "react";
 
+import "./RecommendedSong.scss";
+
+import spotifyIcon from "../../assets/icons/Spotify_Icon_RGB_Green.png";
+
 const RecommendedSong = ({ song }) => {
-  console.log(song);
   return (
-    <div>
-      <p>{song.name}</p>
-      <p>{song.artists[0].name}</p>
-      <p>{song.album.name}</p>
+    <div className="recomm-song-wrapper">
+      <img
+        src={song.album.images[0].url}
+        alt="Album Cover"
+        className="recomm-song__album-cover"
+      />
+      <div className="recomm-song__text-wrapper">
+        <h3 className="recomm-song__name"> {song.name}</h3>
+        <p className="recomm-song__artist">{song.artists[0].name}</p>
+        <p className="recomm-song__album">{song.album.name}</p>
+      </div>
+      <a href="https://open.spotify.com/" className="recomm-song__spotify-link">
+        <img
+          src={spotifyIcon}
+          alt="Spotify Icon"
+          className="recomm-song__attribution"
+        />
+      </a>
     </div>
   );
 };
